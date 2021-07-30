216ddea7222272b585e1cda8fcc5d350fb1d82fa
talebhammoude <taleb.hammoude@outlook.com>
2021-07-30 16:47:53 +0200

    assignment state och event

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,37 @@
+import React, {useState} from 'react';
 import './Expenses.css';
 import ExpenseItem from '../ExpenseItem/ExpenseItem';
 import Card from '../Card/Card';
+import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+
 
 
 function Expenses (props) {
+
+    const [currentDataFromFilterComp, setDataFromFilterComp] = useState('2022');
+
+    const saveDataFromFilterCompInAState = (valueFromFilterComp) => {
+      setDataFromFilterComp(valueFromFilterComp);
+
+    }
+
+    console.log(currentDataFromFilterComp);
+
+
     return (
-        <Card className="expenses">
-      <ExpenseItem ti={props.item[0].title} am={props.item[0].amount} da={props.item[0].date} />
-      <ExpenseItem ti={props.item[1].title} am={props.item[1].amount} da={props.item[1].date} />
-      <ExpenseItem ti={props.item[2].title} am={props.item[2].amount} da={props.item[2].date} />
-      <ExpenseItem ti={props.item[3].title} am={props.item[3].amount} da={props.item[3].date} />
-        </Card>
+      <div>
+      <Card className="expenses">
+        <ExpensesFilter saveInAStateInExpComp={saveDataFromFilterCompInAState} />
+
+        <ExpenseItem ti={props.item[0].title} am={props.item[0].amount} da={props.item[0].date} />
+        <ExpenseItem ti={props.item[1].title} am={props.item[1].amount} da={props.item[1].date} />
+        <ExpenseItem ti={props.item[2].title} am={props.item[2].amount} da={props.item[2].date} />
+        <ExpenseItem ti={props.item[3].title} am={props.item[3].amount} da={props.item[3].date} />
+      </Card>
+      </div>
+
     );
 }
 
 
-export default Expenses
+export default Expenses;
---
+++ src/components/ExpensesFilter/ExpensesFilter.js
@@ -0,0 +1,32 @@
+import React , {useState} from 'react';
+
+import './ExpensesFilter.css';
+
+function ExpensesFilter  (props)  {
+
+    const [currentDropdownValue, setDropdownValue] = useState('2022'); 
+
+    const listenToDropdownData = (event) => {
+        setDropdownValue(event.target.value);
+        
+      }
+
+    props.saveInAStateInExpComp(currentDropdownValue);
+
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={listenToDropdownData}>
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
